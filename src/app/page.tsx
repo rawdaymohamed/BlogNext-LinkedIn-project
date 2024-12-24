abeb7b3223622351f52a556f68faf7095493f64f
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-12-24 15:29:36 +0200

    update style in home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-50">
       <div className="container mx-auto p-4 relative">
-        <div className="bg-white border-2 border-purple-100 rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+        <div className="bg-white rounded-lg p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="flex flex-col justify-center">
             <h1 className={`text-4xl font-bold mb-4 ${styles.text_wisteria}`}>Welcome!</h1>
             <p className="text-lg text-gray-700 mb-4">
@@ -16,7 +16,7 @@ export default function Home() {
               href="/blog/posts" 
               className={`outline outline-1 outline-offset-2 border-purple-700 hover:text-white py-2 px-4 rounded hover:bg-purple-800 md:w-auto ${styles.text_wisteria} ${styles.fit_content}`}
             >
-              Go to Blog!
+              Go to Blog
             </a>
           </div>
           <div className="relative flex justify-center items-center">
