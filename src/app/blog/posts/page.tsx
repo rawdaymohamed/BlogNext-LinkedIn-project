d1d5fc12869d7b38e65a645b959d44d036c32583
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-12-24 16:25:32 +0200

    add postgres

--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,14 +1,14 @@
 import { posts } from "@/app/lib/placeholder-data";
 import Post from "@/app/ui/components/posts/Post";
-export default function Page() {
+import { connectToDB } from "@/app/lib/data";
+export default async function Page() {
+  const client = await connectToDB();
   return (
     <>
-    <h2>Posts</h2>
+      {client && <p className="text-green-500">Connected to database</p>}
+      <h2>Posts</h2>
       {posts.map((post) => (
-        <Post
-          key={post.id}
-          {...post}
-        />
+        <Post key={post.id} {...post} />
       ))}
     </>
   );
