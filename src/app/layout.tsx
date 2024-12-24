489b19b7b2e55b067cda60e6bfc9fa1656e1b287
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2024-12-24 10:54:56 +0200

    Chore: Add images for mobile and desktop and change font to roboto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Roboto } from "next/font/google";
 import "@/app/ui/styles/globals.css"
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({ subsets: ["latin"] , weight:'400'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import styles from "@/app/ui/styles/home.module.css"
+import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex items-center justify-center min-h-screen bg-gray-50">
@@ -12,13 +13,31 @@ export default function Home() {
               nisi, senectus pharetra, ac porttitor orci.
             </p>
             <a
-              href="/blog/posts"
+              href="/blog/posts" 
               className={`outline outline-1 outline-offset-2 border-purple-700 hover:text-white py-2 px-4 rounded hover:bg-purple-800 md:w-auto ${styles.text_wisteria} ${styles.fit_content}`}
             >
               Go to Blog
             </a>
           </div>
+          <div className="relative flex justify-center items-center">
+            {/* Image */}
+            <Image
+              src="/image-desktop.jpg"
+              width={1000}
+              height={760}
+              className="hidden rounded-lg md:block z-10"
+              alt="Screenshots of the dashboard project showing desktop version"
+            />
+            <Image
+              src="/image-mobile.jpg"
+              width={560}
+              height={620}
+              className="block rounded-md md:hidden"
+              alt="Screenshot of the dashboard project showing mobile version"
+            />
+          </div>
         </div>
+      
       </div>
       <div className={`hidden md:block absolute top-0 right-0 bottom-0 left-2/3 z-0 ${styles.bg_wisteria}`}></div>
     </main>
